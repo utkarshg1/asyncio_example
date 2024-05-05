0a7078cd2ffe4c78714203eb3073bf22e11f4b0f
Utkarsh Gaikwad <gaikwadujg@gmail.com>
2024-05-05 03:34:24 +0000

    Async function done

---
+++ example.py
@@ -0,0 +1,27 @@
+import time 
+import numpy as np 
+import asyncio
+
+async def get_squares(n:float)->float:
+    """
+    Gets square of a given number
+    """
+    await asyncio.sleep(1)
+    return n**2
+
+async def main():
+    t1 = time.time()
+    print('Program Started')
+    a = np.random.normal(loc=10, scale=1, size=1000)
+    tasks = []
+    for i in a:
+        task = asyncio.create_task(get_squares(i))
+        tasks.append(task)
+    for j in tasks:
+        await j
+    print('Done!')
+    t2 = time.time()
+    print(f'Time required to complete the task : {(t2-t1):.2f} s')
+
+if __name__=='__main__':
+    asyncio.run(main())
